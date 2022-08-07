02b77b8e4bba0bee63ef24e747b3782cade09142
Lets Do Engineering <103782995+letsdoengineering@users.noreply.github.com>
2022-08-07 11:27:11 +0100

    make download to csv format

--- src/components/sections/proj/DownloadSection.tsx
+++ src/components/sections/proj/DownloadSection.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import { format } from 'date-fns'
-
-import Text from '../../basics/Text/Text'
-import BreadcrumbNav from '../../basics/BreadcrumbNav/BreadcrumbNav'
 import { navigate } from 'gatsby'
+
+import BreadcrumbNav from '../../basics/BreadcrumbNav/BreadcrumbNav'
+import Text from '../../basics/Text/Text'
+import * as downloadStyles from './Download.module.css'
+
+function flattenObject(obj: Record<string, any>): Record<string, string> {
+  const toReturn = {}
+
+  for (const item in obj) {
+    if (!obj.hasOwnProperty(item)) continue
+
+    if (typeof obj[item] == 'object' && obj[item] !== null) {
+      const flatObject = flattenObject(obj[item])
+      for (const x in flatObject) {
+        if (!flatObject.hasOwnProperty(x)) continue
+
+        toReturn[item + '.' + x] = flatObject[x]
+      }
+    } else {
+      toReturn[item] = obj[item]
+    }
+  }
+  return toReturn
+}
+
+function convertToCSV(arr): string[] {
+  const array = [Object.keys(arr[0])].concat(arr)
+
+  return array
+    .map((it) => {
+      return Object.values(it).toString()
+    })
+    .join('\n')
+}
 
 const DownloadSection: React.FC = () => {
   if (typeof window == 'undefined') return null
@@ -25,27 +56,35 @@ const DownloadSection: React.FC = () => {
     const groupName = data.details.group
     const todaysDate = format(new Date(), 'yyyy-MM-dd')
     const fileName = todaysDate + '_' + groupName + '_' + childName
+    const flatData = flattenObject(data)
+
+    console.log('Data flattened:', flatData)
+    const csv = convertToCSV([flatData])
+    console.log('And converted to csv:', csv)
     return (
       <>
         <BreadcrumbNav urlList={[{ url: '/', label: 'RE-START' }, { label: 'Download' }]} />
-        <p>
-          <a
-            href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(data))}`}
-            download={`${fileName}.csv`}
-          >
-            <Text size='XL'>{`DOWNLOAD data: "${fileName}.json"`}</Text>
-          </a>
-        </p>
-        <p>{JSON.stringify(data)}</p>
+        <div className={downloadStyles.content}>
+          <Text>Please remember to download the data by clicking this link:</Text>
+          <div className={downloadStyles.downloadLinkWrapper}>
+            <a
+              href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(data))}`}
+              download={`${fileName}.csv`}
+            >
+              <Text size='XL' className={downloadStyles.downloadLink}>{`${fileName}.json`}</Text>
+            </a>
+          </div>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <BreadcrumbNav urlList={[{ url: '/', label: 'RE-START' }, { label: 'Download' }]} />
+        <p>retrieving data....</p>
       </>
     )
   }
-  return (
-    <>
-      <BreadcrumbNav urlList={[{ url: '/', label: 'RE-START' }, { label: 'Download' }]} />
-      <p>retrieving data....</p>
-    </>
-  )
 }
 
 export default DownloadSection
